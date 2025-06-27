e66265d5bd0334c6a3c4e11152efef8ea6625bc7
tech-haxz <ranktopno1@gmail.com>
2025-06-27 12:40:10 +0530

    removed commented code!

--- OIBSIP - Task 4/Login Authentication/authentication_project/authentication_project/views.py
+++ OIBSIP - Task 4/Login Authentication/authentication_project/authentication_project/views.py
@@ -13,35 +13,11 @@ def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            # user = User.objects.create_user(
-            #     username=form.cleaned_data['username'],
-            #     email=form.cleaned_data['email'],
-            #     password=form.cleaned_data['password1']
-            # )
             user = form.save(commit=False)
-            # user.set_password(form.cleaned_data['password1'])  # Hash the password
+            user.set_password(form.cleaned_data['password1'])  # Hash the password
             user.save()
             messages.success(request, "Registration successful!")
             return redirect('login')
     else:
         form = UserRegistrationForm()
     return render(request, 'registration/register.html', {'form': form})
-
-
-# def login_view(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data['username']
-#             password = form.cleaned_data['password']
-#             user = authenticate(username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 messages.success(request, "Login successful!")
-#                 return redirect('home')
-#             else:
-#                 messages.error(request, "Invalid username or password.")
-#     else:
-#         form = UserLoginForm()
-#     return render(request, 'accounts/login.html', {'form': form})
-
